677feaf4870969f73a600924bc3ce795a9ed1ba9
sandipbhandari <bhandarisandip882@gmail.com>
2022-02-21 14:02:12 +0545

    login form for game

---
+++ app/src/main/java/myapp/game/tictactoe/Ingame.java
@@ -0,0 +1,14 @@
+package myapp.game.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ingame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingame);
+    }
+}
--- app/src/main/java/myapp/game/tictactoe/MainActivity.java
+++ app/src/main/java/myapp/game/tictactoe/MainActivity.java
@@ -2,9 +2,14 @@ package myapp.game.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +20,27 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+        final EditText playerone = findViewById(R.id.playerone);
+        final EditText playertwo = findViewById(R.id.playertwo);
+        final Button start=findViewById(R.id.start);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String getplayeroneName = playerone.getText().toString();
+                final String getplayertwoName = playertwo.getText().toString();
+                if (getplayeroneName.isEmpty() || getplayertwoName.isEmpty()){
+                    Toast.makeText(MainActivity.this, "please enter player name", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent intent = new Intent(MainActivity.this,Ingame.class);
+                    intent.putExtra("playerone",getplayeroneName);
+                    intent.putExtra("playerTwo",getplayertwoName);
+                    startActivity(intent);
+                }
+
+            }
+        });
     }
 }
